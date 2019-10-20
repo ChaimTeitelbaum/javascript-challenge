1ea15eecf8b038ba468ee334e7f8f89304203f25
Chaim Teitelbaum <47922870+ChaimTeitelbaum@users.noreply.github.com>
2019-10-20 03:31:20 +0300

    Add files via upload

--- UFO-level-1 and UFO-level-2/Level 2/static/js/app.js
+++ UFO-level-1 and UFO-level-2/Level 2/static/js/app.js
@@ -1,64 +1,75 @@
-// UFO dataset of sightings by Chaim Teitelbaum Colombie Engineering BCS - October 2019 - New York 
-
 // from data.js
-var tData = data;
+var tableData = data;
 
-// function to load dataset
-function tLoad(ufoData) {
-  var tbody = d3.select("tbody");
-  ufoData.forEach((ufoDataset) => {
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+data.forEach((sight) => {
     var row = tbody.append("tr");
-    Object.entries(ufoDataset).forEach(([key, value]) => {
+    Object.entries(sight).forEach(([key, value]) => {
       var cell = row.append("td");
-      cell.html(value);
+      cell.text(value);
     });
   });
-};
 
-// Function to clear old data make room for new data
-function deltbody() {
-  d3.select("tbody")
-    .selectAll("tr").remove()
-    .selectAll("td").remove();
-};
+  var selectedSearch = "datetime";
+  function getData(dataset) {
 
-// start with showing full UFO dataset
-console.log(tData);
-tLoad(tData);
+    switch (dataset) {
+    case "datasetdatetime":
+    selectedSearch = "datetime";
+      break;
+    case "datasetcity":
+      selectedSearch = "city";
+      break;
+    case "datasetshape":
+      selectedSearch = "shape";
+      break;
+      case "datasetstate":
+        selectedSearch = "state";
+        break;
+      case "datasetcountry":
+        selectedSearch = "country";
+        break;      
+    default:
+      selectedSearch = "datetime";
+      break;
+    }
 
-// Button to filter dataset
-var button = d3.select("filter-btn");
+    console.log(selectedSearch);
 
-// filter dataset
-button.on("click", function(event) {
-  d3.event.preventDefault();
-  console.log('TST')
-  deltbody();
-  var date = d3.select("#datetime").property("value");
-  
-  // if no date input show full database
-  if (date.trim() === "" ) {
-    var tDataFiltere = tData;
   }
-  else {
-     
-    var tDataFiltered = tData.filter(ufoData => 
-      ufoData.datetime === date.trim());
-  };
-  else {
-     
-    var tDataFiltered = tData.filter(ufoData => 
-      ufoData.City === City.trim());
-  };
-  // if no records found show message 
-  if (tDataFiltered.length == 0) {
-    d3.select("tbody")
-      .append("tr")
-      .append("td")
-        .attr("colspan", 7)
-        .html("<h4>No Records Found</h4>");
-  };
+      console.log(selectedSearch);
+  var button = d3.select("#filter-btn");
 
-  console.log(tDataFiltered);
-  tDisplay(tDataFiltered);
-});
+ 
+  button.on("click", function() {
+  
+
+  var inputElement = d3.select("#datetime");
+  console.log(inputElement);
+  
+
+  var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    //console.log(data);
+    var filteredData = data.filter(dataItem => dataItem[selectedSearch] === inputValue);
+  console.log(filteredData)
+  
+  function deltbody() {
+    d3.select("tbody")
+      .selectAll("tr").remove()
+      .selectAll("td").remove();
+   };
+  deltbody();
+  
+  filteredData.forEach((sight) => {
+      var row = tbody.append("tr");
+      Object.entries(sight).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  });
+
